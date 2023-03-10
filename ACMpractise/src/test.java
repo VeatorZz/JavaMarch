8e2f42c9d3c669714e41d8a9cb7d8b4399860d62
VeatorZz <Veator_LMY@163.com>
2023-03-10 14:40:09 +0800

    0310

---
+++ ACMpractise/src/test.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            System.out.println();
+            System.out.println(a+b);
+        }
+
+    }
+}
