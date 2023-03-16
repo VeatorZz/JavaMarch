ce8ea542a78ef9210ba7f09f2e45ceb00e8a4307
VeatorZz <Veator_LMY@163.com>
2023-03-16 19:25:10 +0800

    0316

---
+++ pythonProject/main.py
@@ -0,0 +1,22 @@
+import csv
+
+import numpy
+import openpyxl as openpyxl
+import pandas as pd
+
+# data = pd.DataFrame({'score': [61, 66, 68, 73, 77, 78, 85, 88, 91]})
+# n = data.score.size
+# step = int(n/3)
+# cut = data.score.sort_values().ildc[0::step]
+# pd.cut(data.score,cut,labels=["low","middle","high"])
+#
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a[0::3]
+
+#
+# data = pd.read_table("D:/pythonProject/rz1.txt", sep = "," , Header =None)
+# print(data)
+from pandas import DataFrame
+df=DataFrame;
+df.to_scv(D)
