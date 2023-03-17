e94906b9db95e670c136ee8e120aa5d5d85c8d4d
VeatorZz <Veator_LMY@163.com>
2023-03-17 17:28:55 +0800

    Update asd.java

--- practice/src/asd.java
+++ practice/src/asd.java
@@ -1,25 +1,14 @@
 import java.util.Scanner;
+
 public class asd {
-    public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        int f =scanner.nextInt();
-        System.out.println("第一题:"+f1(f));
-        System.out.println("第二题:"+f2(f));
 
-    }
-    public static int f1(int n ){
-        if(n==1){
-            return 1;
-        }
-            return n*f1(n-1);
-        }
+    public static void main(String args[]){
+        int m =100;
 
-    public static int f2(int n){
-
-        if(n==0){
-            return 0;
-        }
-        return f1(n)+f2(n-1);
-    }
-
+        while(m !=0){}
+        if(m)
 }
+
+
+
+
