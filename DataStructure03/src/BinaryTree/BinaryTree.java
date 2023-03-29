6ffcdc98107a8e9a3f342b5cd330d5556f9d4892
VeatorZz <Veator_LMY@163.com>
2023-03-30 00:31:24 +0800

    123

--- DataStructure03/src/BinaryTree/BinaryTree.java
+++ DataStructure03/src/BinaryTree/BinaryTree.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class BinaryTree {
     public class TreeNode{
         public char val;
@@ -10,7 +12,7 @@ public class BinaryTree {
         }
     }
     public  TreeNode root;
-    public  void createTree(){
+    public TreeNode createTree(){
         TreeNode A = new TreeNode('A');
         TreeNode B = new TreeNode('B');
         TreeNode C = new TreeNode('C');
@@ -27,6 +29,7 @@ public class BinaryTree {
         C.right=G;
         E.right=H;
         root =A;
+        return A;
     }
 
     void preOrder(TreeNode root){
@@ -46,6 +49,133 @@ public class BinaryTree {
        postOrder(root.left);
        postOrder(root.right);
         System.out.println(root.val+" ");
+    }
+    public static int nodeSize;
+    void size1(TreeNode root){
+        if(root ==null) return;
+        nodeSize++;
+        size1(root.left);
+        size1(root.right);
+    }
+    int size2(TreeNode root){
+        if(root ==null ) return 0;
+        return size2(root.left)+size2(root.right)+1;
+    }
+
+
+
+    public  static  int leafSize =0;
+    public void getLeafNodeCount1(TreeNode root){
+        if(root==null)return ;
+        if(root.left ==null&& root.right ==null){
+            leafSize++;
+        }
+        getLeafNodeCount1(root.left);
+        getLeafNodeCount1(root.right);
+    }
+
+
+    public int getLeafNodeCount2(TreeNode root){
+        if(root==null)return 0;
+        if(root.left ==null&& root.right ==null){
+            return 1;
+        }
+        return getLeafNodeCount2(root.left)+getLeafNodeCount2(root.right);
+    }
+    public int getKLevelNodeCount(TreeNode root,int k){
+        if(root == null)return 0;
+        if (k==1) return 1;
+        return getKLevelNodeCount(root.left,k-1)+getKLevelNodeCount(root.right, k-1);
+    }
+
+
+    public  int getHigh(TreeNode root){
+//        if(root.left == null && root.right==null){
+//            return 0;
+//        }
+        if(root ==null) return 0;
+        return Math.max(getHigh(root.left),getHigh(root.right))+1;
+    }
+
+    public TreeNode find(TreeNode root,char val){
+        if(root == null) return  null;
+        if(root.val ==val){
+            return root;
+        }
+        TreeNode ret=find(root.left,val);
+        if(ret!= null) return ret;
+         ret=find(root.right,val);
+        if(ret!= null) return ret;
+        return  null;
+    }
+
+    public  void levelOrder(TreeNode root){
+        if(root == null) return ;
+        Queue<TreeNode> queue =new LinkedList<>();
+            queue.offer(root);
+            while(!queue.isEmpty()){
+                TreeNode cur =queue.poll();
+                System.out.print(cur.val+" ");
+                if(cur.left !=null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right !=null){
+                    queue.offer(cur.right);
+                }
+            }
+
+
+    }
+    public List<List<Character>> levelOrder1(TreeNode root) {
+        List<List<Character>> ret =new ArrayList<>();
+        if(root == null) return ret ;
+        Queue<TreeNode> queue =new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Character> list =new ArrayList<>();
+            while (size != 0){
+                TreeNode cur =queue.poll();
+                list.add(cur.val);
+                size--;
+                if(cur.left !=null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right !=null){
+                    queue.offer(cur.right);
+                }
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+    public List<Character> postorderTraversal(TreeNode root) {
+        List<Character> list =new ArrayList<>();
+        if(root !=null) return list;
+        Stack<TreeNode> stack =new Stack<>();
+        TreeNode cur =root;
+        while(cur!=null){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
 
--- DataStructure03/src/BinaryTree/test.java
+++ DataStructure03/src/BinaryTree/test.java
@@ -1,8 +1,21 @@
 package BinaryTree;
 
+import javax.swing.tree.TreeNode;
+
 public class test {
     public static void main(String[] args) {
         BinaryTree binaryTree =new BinaryTree();
-        binaryTree.createTree();
+        BinaryTree.TreeNode root=binaryTree.createTree();
+        binaryTree.size1(root);
+        System.out.println(BinaryTree.nodeSize);
+        System.out.println(binaryTree.size2(root));;
+        binaryTree.getLeafNodeCount1(root);
+        System.out.println(BinaryTree.leafSize);
+        System.out.println(binaryTree.getLeafNodeCount2(root));
+        System.out.println(binaryTree.getKLevelNodeCount(root,4));
+        System.out.println(binaryTree.getHigh(root));
+        System.out.println(binaryTree.find(root,'C'));
+        System.out.println(binaryTree.levelOrder1(root));
+
     }
 }
