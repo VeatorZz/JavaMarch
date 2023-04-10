0b3d7fc20c279f9b14735401bede1ffade6f77d0
VeatorZz <Veator_LMY@163.com>
2023-04-10 08:20:21 +0800

    123

--- DataStructure03/src/BinarySearchTree/BinarySearchTree.java
+++ DataStructure03/src/BinarySearchTree/BinarySearchTree.java
@@ -1,70 +1,124 @@
 package BinarySearchTree;
 
 public class BinarySearchTree {
-    static class TreeNode{
-        public  int key;
+    static class TreeNode {
+        public int key;
         public TreeNode left;
         public TreeNode right;
-        TreeNode(int key){
-            this.key=key;
+
+        TreeNode(int key) {
+            this.key = key;
         }
     }
-
-
-
 
     public TreeNode root;
-    public boolean insert(int key){
-    if(root ==null){
-        root =new TreeNode(key);
+
+    /**
+     *
+     * @param key
+     */
+    public boolean insert(int key) {
+        if(root == null) {
+            root = new TreeNode(key);
+            return true;
+        }
+
+        TreeNode cur = root;
+        TreeNode parent = null;
+
+        while (cur != null) {
+            if(cur.key < key) {
+                parent = cur;
+                cur = cur.right;
+            }else if(cur.key > key) {
+                parent = cur;
+                cur = cur.left;
+            }else {
+                return false;
+            }
+        }
+        TreeNode node = new TreeNode(key);
+        //当代码执行到这里之后。cur == null
+        if(parent.key < key) {
+            parent.right = node;
+        }else {
+            parent.left = node;
+        }
         return true;
     }
-    TreeNode cur =root;
-    TreeNode parent =null;
-    while(cur != null){
-        if(cur.key<key){
-            parent =cur;
-            cur =cur.right;
-        }else if(cur.key>key){
-            parent =cur;
-            cur =cur.right;
-        }else{
-            return false;
-        }
-    }
 
-    TreeNode node = new TreeNode(key);
-        //走到这root(cur)是空的
-    if(parent.key<key){
-        parent.right =node;
-    }else{
-        parent.left =node;
-    }
-    return true;
-
-
-    }
-    //中序遍历即可
-    public void inorder(TreeNode root){
-        if(root ==null) return;
-        inorder(root.left);
-        System.out.println(root.key+" ");
-        inorder(root.right);
-    }
-
-
-    public TreeNode search(int key){
-        TreeNode cur =root;
-        while(cur != null){
-            if(cur.key ==key){
+    public TreeNode search(int key) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if(cur.key == key) {
                 return cur;
-            }else if(cur.key<key){
-                cur =cur.left;
-            }else{
-                cur =cur.right;
+            }else if(cur.key < key) {
+                cur = cur.right;
+            }else {
+                cur = cur.left;
             }
         }
         return null;
+    }
+
+    public boolean remove(int key) {
+        TreeNode cur = root;
+        TreeNode parent = null;
+        while (cur != null) {
+            if(cur.key < key) {
+                parent = cur;
+                cur = cur.right;
+            }else if(cur.key == key) {
+                //这里开始删除！
+                removeNode(parent,cur);
+                return true;
+            }else {
+                parent = cur;
+                cur = cur.left;
+            }
+        }
+        return false;
+    }
+
+    private void removeNode(TreeNode parent,TreeNode cur) {
+        if(cur.left == null) {
+            if(cur == root) {
+                root = cur.right;
+            }else if(cur == parent.left) {
+                parent.left = cur.right;
+            }else {
+                parent.right = cur.right;
+            }
+        }else if(cur.right == null) {
+            if(cur == root) {
+                root = cur.left;
+            }else if(cur == parent.left) {
+                parent.left = cur.left;
+            }else {
+                parent.right = cur.left;
+            }
+        }else {
+            TreeNode targetParent = cur;
+            TreeNode target = cur.right;
+            while (target.left != null) {
+                targetParent = target;
+                target = target.left;
+            }
+            cur.key = target.key;
+            if(target == targetParent.left) {
+                targetParent.left = target.right;
+            }else {
+                targetParent.right = target.right;
+            }
+        }
+    }
+
+    //中序遍历即可
+    public void inorder(TreeNode root) {
+        if(root == null) return;
+        inorder(root.left);
+        System.out.print(root.key+" ");
+        inorder(root.right);
     }
 
 
--- DataStructure03/src/BinarySearchTree/Test.java
+++ DataStructure03/src/BinarySearchTree/Test.java
@@ -1,14 +1,30 @@
 package BinarySearchTree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
     public static void main(String[] args) {
+
+        Map<String,Integer> map = new HashMap<>();
+        map.put("hello",3);
+        map.put("abcd",4);
+        map.put("bit", 10);
+        System.out.println(map);
         BinarySearchTree binarySearchTree =new BinarySearchTree();
         int[] array={10,4,29,35,18,27,5,13};
         for (int i = 0; i < array.length; i++) {
             binarySearchTree.insert(array[i]);
         }
-        System.out.println();
-        BinarySearchTree.TreeNode  ret =binarySearchTree.search(18);
-        System.out.println(ret.key);
+
+     BinarySearchTree.TreeNode  ret =binarySearchTree.search(18);
+        try{
+            System.out.println(ret.key);
+        }catch (NullPointerException e ){
+            System.out.println("没有你要找的节点");
+            e.printStackTrace();
+        }
+      binarySearchTree.remove(10);
+        System.out.println("测试");
     }
 }
