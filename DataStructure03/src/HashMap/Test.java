5fe536bb1a7af210f2cb757548aa553968aebbf0
VeatorZz <Veator_LMY@163.com>
2023-04-12 23:52:25 +0800

    Update Test.java

--- DataStructure03/src/HashMap/Test.java
+++ DataStructure03/src/HashMap/Test.java
@@ -1,12 +1,8 @@
 package HashMap;
 
 public class Test {
-    public static void main(String[] args) {
-        HashBuck hashBuck =new HashBuck();
-        hashBuck.put(1,99);
-        hashBuck.put(4,99);
-        hashBuck.put(5,16);
-        hashBuck.put(6,99);
-        System.out.println("ds");
-    }
+
 }
+
+    //输出该数组
+
